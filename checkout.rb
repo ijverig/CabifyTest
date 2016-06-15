57bd6a8151a547eae4cc7a5c492911511fe143c6
Daniel Schmidt <silveirinha@gmail.com>
2016-06-15 15:19:30 +0200

    added checkout pricing rules

--- checkout.rb
+++ checkout.rb
@@ -11,11 +11,21 @@ class Checkout
     end
 
     def total
+        # all items here have already been validated
+
         total = 0
 
         @items.each do |item, quantity|
             puts "#{item}: #{quantity}"
-            total += @prices[item] * quantity
+
+            total += case item
+            when "VOUCHER"
+                5 * (quantity / 2.0).ceil
+            when "TSHIRT"
+                (quantity >= 3 ? 19 : 20) * quantity
+            when "MUG"
+                7.50 * quantity
+            end
         end
 
         puts "Total: %.2f" % [total]
