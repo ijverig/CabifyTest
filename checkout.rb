2b17edb21a89299360c437763b5a534e20ec3e81
Daniel Schmidt <silveirinha@gmail.com>
2016-06-15 14:59:01 +0200

    added checkout basic logic

--- checkout.rb
+++ checkout.rb
@@ -1,9 +1,23 @@
 class Checkout
+    def initialize(prices)
+        @prices = prices
+        @items = Hash.new(0)
+    end
+
     def scan(item)
-        puts item
+        # TODO: check if item is valid
+
+        @items[item] += 1
     end
 
     def total
-        puts "Total: 76.40"
+        total = 0
+
+        @items.each do |item, quantity|
+            puts "#{item}: #{quantity}"
+            total += @prices[item] * quantity
+        end
+
+        puts "Total: %.2f" % [total]
     end
 end
--- checkout_test.rb
+++ checkout_test.rb
@@ -1,7 +1,14 @@
 require './checkout'
 
+# test set setup
+prices = {
+    "VOUCHER"   => 5.00,
+    "TSHIRT"    => 20.00,
+    "MUG"       => 7.50
+}
+
 # test case 1
-co = Checkout.new
+co = Checkout.new(prices)
 ["VOUCHER", "TSHIRT", "MUG"].each do |item|
     co.scan item
 end
@@ -10,7 +17,7 @@ puts "(Should be 32.50)"
 puts
 
 # test case 2
-co = Checkout.new
+co = Checkout.new(prices)
 ["VOUCHER", "TSHIRT", "VOUCHER"].each do |item|
     co.scan item
 end
@@ -19,7 +26,7 @@ puts "(Should be 25.00)"
 puts
 
 # test case 3
-co = Checkout.new
+co = Checkout.new(prices)
 ["TSHIRT", "TSHIRT", "TSHIRT", "VOUCHER", "TSHIRT"].each do |item|
     co.scan item
 end
@@ -28,7 +35,7 @@ puts "(Should be 81.00)"
 puts
 
 # test case 4
-co = Checkout.new
+co = Checkout.new(prices)
 ["VOUCHER", "TSHIRT", "VOUCHER", "VOUCHER", "MUG", "TSHIRT", "TSHIRT"].each do |item|
     co.scan item
 end
